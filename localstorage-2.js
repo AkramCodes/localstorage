7139d7664be4cc3ea25ad14fd2bb61b305d5ba98
AkramCodes <sbntech72@gmail.com>
2024-01-12 18:21:42 +0530

    some correction

--- localstorage-2.js
+++ localstorage-2.js
@@ -64,7 +64,7 @@ function printValues(){
    ArrayX.forEach(function(value,index){
     addTaskList.innerHTML +=`
     <div class="mainclass"> 
-    <h2 class="heading">First User </h2>   
+       
     <div class="input"> <span> Your Name is </span> <input type="text" class="inputclass" value="${value.name}" readonly> </div>
     <div class="input"> <span> Your Name is </span> <input type="text" class="inputclass" value="${value.email}" readonly> </div>
     <div class="input"> <span> Your Name is </span> <input type="text" class="inputclass" value="${value.phone}" readonly> </div>
